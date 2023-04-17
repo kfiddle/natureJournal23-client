fda1edea242bccfe06ab1b93e1dc7572434ce2fc
kfiddle <kenjfiddle@gmail.com>
2023-04-17 14:15:38 -0400

    beginning stuff

---
+++ src/homepage/Homepage.js
@@ -0,0 +1,11 @@
+import styles from "./Homepage.module.css";
+const Homepage = () => {
+  return (
+    <div className={styles.outerContainer}>
+      <div>HOMEPAGE</div>
+      <button>Add Entry</button>
+    </div>
+  );
+};
+
+export default Homepage;
